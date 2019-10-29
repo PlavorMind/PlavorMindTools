7cd02366a992cd49dff0ffd833cdca623bc4fbc4
PlavorSeol <pseol2190@outlook.com>
2019-10-30 06:51:50 +0900

    Remove $wgPMTMBoxCSSExemptSkins

--- hooks.php
+++ hooks.php
@@ -5,8 +5,7 @@ if (!defined("MEDIAWIKI"))
 
 class PlavorMindToolsHooks
 {public static function onBeforePageDisplay(OutputPage $out,Skin $skin)
-  {global $wgPMTMBoxCSSExemptSkins;
-  $out->addModuleStyles(["plavormindtools_main"]);
+  {$out->addModuleStyles(["plavormindtools_main"]);
   if ($skin->getSkinName()=="liberty")
     {$out->addModuleStyles("plavormindtools_liberty_fix");}
   }
