88670bbfa3a46738226c4186827317c2b4f8108c
PlavorSeol <pseol2190@outlook.com>
2020-02-07 19:25:54 +0900

    Update NoActionsOnNonEditableHooks.php

--- includes/NoActionsOnNonEditableHooks.php
+++ includes/NoActionsOnNonEditableHooks.php
@@ -7,11 +7,12 @@ class NoActionsOnNonEditableHooks
 {public static function ongetUserPermissionsErrors($title,$user,$action,&$result)
   {global $wgPMTFeatureConfig;
   if ($wgPMTFeatureConfig["NoActionsOnNonEditable"]["enable"])
-    {//$action should be checked first to avoid "Allowed memory size of N bytes exhausted" error
-    if ($action=="delete"&&!MediaWikiServices::getInstance()->getPermissionManager()->userCan("edit",$user,$title,"quick"))
+    {$PermissionManager=MediaWikiServices::getInstance()->getPermissionManager();
+    //$action should be checked first to avoid "Allowed memory size of N bytes exhausted" error
+    if ($action=="delete"&&!$PermissionManager->userCan("edit",$user,$title,"quick"))
       {$result=["noactionsonnoneditable-cannotdeletecannotedit"];
       return false;}
-    if ($wgPMTFeatureConfig["NoActionsOnNonEditable"]["HideMoveTab"]&&$action=="move"&&!MediaWikiServices::getInstance()->getPermissionManager()->userCan("edit",$user,$title,"quick"))
+    if ($wgPMTFeatureConfig["NoActionsOnNonEditable"]["HideMoveTab"]&&$action=="move"&&!$PermissionManager->userCan("edit",$user,$title,"quick"))
       {$result=["noactionsonnoneditable-cannotmovecannotedit"];
       return false;}
     }
