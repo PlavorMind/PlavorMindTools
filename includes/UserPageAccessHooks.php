33adf2060eb2db2648db78e61f809f9652fe2409
PlavorSeol <pseol2190@outlook.com>
2020-02-22 02:18:47 +0900

    Use config object

--- includes/UserPageAccessHooks.php
+++ includes/UserPageAccessHooks.php
@@ -1,35 +1,38 @@
 <?php
 use MediaWiki\MediaWikiServices;
-if (!defined("MEDIAWIKI"))
-{exit("This is not a valid entry point.");}
 
 class UserPageAccessHooks
 {public static function ongetUserPermissionsErrors($title,$user,$action,&$result)
-  {global $wgPMTFeatureConfig;
-  if ($wgPMTFeatureConfig["UserPageAccess"]["enable"])
-    {if ($action=="edit"&&$title->getNamespace()==NS_USER)
-      {if (!($title->getRootText()==$user->getName()||MediaWikiServices::getInstance()->getPermissionManager()->userHasRight($user,"editotheruserpages")))
+  {$config=MediaWikiServices::getInstance()->getConfigFactory()->makeConfig("plavormindtools");
+  if ($config->get("PMTFeatureConfig")["UserPageAccess"]["enable"])
+    {if ($action=="edit" && $title->getNamespace()==NS_USER)
+      {if (!($title->getRootText()==$user->getName() || MediaWikiServices::getInstance()->getPermissionManager()->userHasRight($user,"editotheruserpages")))
         {$result=["userpageaccess-cannotedituserpage"];
         return false;}
       }
     }
   }
 public static function onMovePageCheckPermissions(Title $oldTitle,Title $newTitle,User $user,$reason,Status $status)
-  {global $wgPMTFeatureConfig;
-  if ($wgPMTFeatureConfig["UserPageAccess"]["enable"])
+  {$config=MediaWikiServices::getInstance()->getConfigFactory()->makeConfig("plavormindtools");
+  if ($config->get("PMTFeatureConfig")["UserPageAccess"]["enable"])
     {$PermissionManager=MediaWikiServices::getInstance()->getPermissionManager();
-    if ($oldTitle->getNamespace()==NS_USER&&$oldTitle->getRootText()==$user->getName()&&!$PermissionManager->userHasRight($user,"move")&&$PermissionManager->userHasRight($user,"moveownuserpages")&&$status->hasMessage("movenotallowed")&&count($status->getErrorsArray())==1)
+    if ($oldTitle->getNamespace()==NS_USER
+    && $oldTitle->getRootText()==$user->getName()
+    && !$PermissionManager->userHasRight($user,"move")
+    && $PermissionManager->userHasRight($user,"moveownuserpages")
+    && $status->hasMessage("movenotallowed")
+    && count($status->getErrorsArray())==1)
       {$status->setOK(true);}
     }
   }
 public static function onTitleQuickPermissions($title,$user,$action,&$errors,$doExpensiveQueries,$short)
-  {global $wgPMTFeatureConfig;
-  if ($wgPMTFeatureConfig["UserPageAccess"]["enable"])
+  {$config=MediaWikiServices::getInstance()->getConfigFactory()->makeConfig("plavormindtools");
+  if ($config->get("PMTFeatureConfig")["UserPageAccess"]["enable"])
     {$PermissionManager=MediaWikiServices::getInstance()->getPermissionManager();
-    if ($title->getNamespace()==NS_USER&&$title->getRootText()==$user->getName())
-      {if ($action=="delete"&&!$PermissionManager->userHasRight($user,"delete")&&$PermissionManager->userHasRight($user,"deleteownuserpages")&&$PermissionManager->userCan("edit",$user,$title,"quick"))
+    if ($title->getNamespace()==NS_USER && $title->getRootText()==$user->getName())
+      {if ($action=="delete" && !$PermissionManager->userHasRight($user,"delete") && $PermissionManager->userHasRight($user,"deleteownuserpages") && $PermissionManager->userCan("edit",$user,$title,"quick"))
         {return false;}
-      if ($action=="move"&&!$PermissionManager->userHasRight($user,"move")&&$PermissionManager->userHasRight($user,"moveownuserpages")&&$PermissionManager->userCan("edit",$user,$title,"quick"))
+      if ($action=="move" && !$PermissionManager->userHasRight($user,"move") && $PermissionManager->userHasRight($user,"moveownuserpages") && $PermissionManager->userCan("edit",$user,$title,"quick"))
         {return false;}
       }
     }
