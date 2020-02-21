b31e4ef39cc76001e2405802b4aae9a988137ee9
PlavorSeol <pseol2190@outlook.com>
2020-02-22 02:25:08 +0900

    Edit comment

--- includes/NoActionsOnNonEditableHooks.php
+++ includes/NoActionsOnNonEditableHooks.php
@@ -6,7 +6,7 @@ class NoActionsOnNonEditableHooks
   {$config=MediaWikiServices::getInstance()->getConfigFactory()->makeConfig("plavormindtools");
   if ($config->get("PMTFeatureConfig")["NoActionsOnNonEditable"]["enable"])
     {$PermissionManager=MediaWikiServices::getInstance()->getPermissionManager();
-    //$action should be checked first to avoid "Allowed memory size of N bytes exhausted" error
+    //$action should be checked before $PermissionManager->userCan to avoid "Allowed memory size of N bytes exhausted" error
     if ($action=="delete" && !$PermissionManager->userCan("edit",$user,$title,"quick"))
       {$result=["noactionsonnoneditable-cannotdeletecannotedit"];
       return false;}
