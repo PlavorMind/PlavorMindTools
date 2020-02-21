4dcb1428760feca7811d1acdb45cfd0b996a7e92
PlavorSeol <pseol2190@outlook.com>
2020-02-22 02:13:41 +0900

    Use config object

--- includes/NoActionsOnNonEditableHooks.php
+++ includes/NoActionsOnNonEditableHooks.php
@@ -1,18 +1,16 @@
 <?php
 use MediaWiki\MediaWikiServices;
-if (!defined("MEDIAWIKI"))
-{exit("This is not a valid entry point.");}
 
 class NoActionsOnNonEditableHooks
 {public static function ongetUserPermissionsErrors($title,$user,$action,&$result)
-  {global $wgPMTFeatureConfig;
-  if ($wgPMTFeatureConfig["NoActionsOnNonEditable"]["enable"])
+  {$config=MediaWikiServices::getInstance()->getConfigFactory()->makeConfig("plavormindtools");
+  if ($config->get("PMTFeatureConfig")["NoActionsOnNonEditable"]["enable"])
     {$PermissionManager=MediaWikiServices::getInstance()->getPermissionManager();
     //$action should be checked first to avoid "Allowed memory size of N bytes exhausted" error
-    if ($action=="delete"&&!$PermissionManager->userCan("edit",$user,$title,"quick"))
+    if ($action=="delete" && !$PermissionManager->userCan("edit",$user,$title,"quick"))
       {$result=["noactionsonnoneditable-cannotdeletecannotedit"];
       return false;}
-    if ($wgPMTFeatureConfig["NoActionsOnNonEditable"]["HideMoveTab"]&&$action=="move"&&!$PermissionManager->userCan("edit",$user,$title,"quick"))
+    if ($config->get("PMTFeatureConfig")["NoActionsOnNonEditable"]["HideMoveTab"] && $action=="move" && !$PermissionManager->userCan("edit",$user,$title,"quick"))
       {$result=["noactionsonnoneditable-cannotmovecannotedit"];
       return false;}
     }
