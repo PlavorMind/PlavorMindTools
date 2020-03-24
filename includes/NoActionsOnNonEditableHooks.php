bd4e4f6b4b7dbd5f430af8bc4c2376032c82b0a9
PlavorSeol <pseol2190@outlook.com>
2020-03-25 07:26:03 +0900

    Optimize

--- includes/NoActionsOnNonEditableHooks.php
+++ includes/NoActionsOnNonEditableHooks.php
@@ -4,16 +4,19 @@ use MediaWiki\MediaWikiServices;
 class NoActionsOnNonEditableHooks
 {public static function ongetUserPermissionsErrors($title,$user,$action,&$result)
   {$config=MediaWikiServices::getInstance()->getConfigFactory()->makeConfig("plavormindtools");
+
   if ($config->get("PMTFeatureConfig")["NoActionsOnNonEditable"]["enable"])
     {$PermissionManager=MediaWikiServices::getInstance()->getPermissionManager();
+
+    $denied_actions=["delete"];
+    if ($config->get("PMTFeatureConfig")["NoActionsOnNonEditable"]["HideMoveTab"])
+      {$denied_actions[]="move";}
     //$action should be checked before $PermissionManager->userCan to avoid "Allowed memory size of N bytes exhausted" error
-    if ($action=="delete" && !$PermissionManager->userCan("edit",$user,$title,"quick"))
-      {$result=["noactionsonnoneditable-cannotdeletecannotedit"];
-      return false;}
-    if ($config->get("PMTFeatureConfig")["NoActionsOnNonEditable"]["HideMoveTab"] && $action=="move" && !$PermissionManager->userCan("edit",$user,$title,"quick"))
-      {$result=["noactionsonnoneditable-cannotmovecannotedit"];
+    if (in_array($action,$denied_actions) && !$PermissionManager->userCan("edit",$user,$title,"quick"))
+      {switch ($action)
+        {default:
+        $result=["noactionsonnoneditable-cannot".$action."cannotedit"];}
       return false;}
     }
   }
 }
-?>
