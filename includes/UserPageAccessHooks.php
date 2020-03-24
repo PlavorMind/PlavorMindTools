c756bdb186e16055c826a37ec5fe392ede1dee3c
PlavorSeol <pseol2190@outlook.com>
2020-03-25 06:47:21 +0900

    Optimize

--- includes/UserPageAccessHooks.php
+++ includes/UserPageAccessHooks.php
@@ -4,18 +4,19 @@ use MediaWiki\MediaWikiServices;
 class UserPageAccessHooks
 {public static function ongetUserPermissionsErrors($title,$user,$action,&$result)
   {$config=MediaWikiServices::getInstance()->getConfigFactory()->makeConfig("plavormindtools");
+
   if ($config->get("PMTFeatureConfig")["UserPageAccess"]["enable"])
-    {if ($action=="edit" && $title->getNamespace()==NS_USER)
-      {if (!($title->getRootText()==$user->getName() || MediaWikiServices::getInstance()->getPermissionManager()->userHasRight($user,"editotheruserpages")))
-        {$result=["userpageaccess-cannotedituserpage"];
-        return false;}
-      }
+    {if ($action=="edit" && $title->getNamespace()==NS_USER && !($title->getRootText()==$user->getName() || MediaWikiServices::getInstance()->getPermissionManager()->userHasRight($user,"editotheruserpages")))
+      {$result=["userpageaccess-cannotedituserpage"];
+      return false;}
     }
   }
 public static function onMovePageCheckPermissions(Title $oldTitle,Title $newTitle,User $user,$reason,Status $status)
   {$config=MediaWikiServices::getInstance()->getConfigFactory()->makeConfig("plavormindtools");
+
   if ($config->get("PMTFeatureConfig")["UserPageAccess"]["enable"])
     {$PermissionManager=MediaWikiServices::getInstance()->getPermissionManager();
+
     if ($oldTitle->getNamespace()==NS_USER
     && $oldTitle->getRootText()==$user->getName()
     && !$PermissionManager->userHasRight($user,"move")
@@ -27,16 +28,19 @@ public static function onMovePageCheckPermissions(Title $oldTitle,Title $newTitl
   }
 public static function onTitleQuickPermissions($title,$user,$action,&$errors,$doExpensiveQueries,$short)
   {$config=MediaWikiServices::getInstance()->getConfigFactory()->makeConfig("plavormindtools");
+
   if ($config->get("PMTFeatureConfig")["UserPageAccess"]["enable"])
     {$PermissionManager=MediaWikiServices::getInstance()->getPermissionManager();
-    if ($title->getNamespace()==NS_USER && $title->getRootText()==$user->getName())
-      {//$action should be checked before $PermissionManager->userCan to avoid "Allowed memory size of N bytes exhausted" error
-      if ($action=="delete" && !$PermissionManager->userHasRight($user,"delete") && $PermissionManager->userHasRight($user,"deleteownuserpages") && $PermissionManager->userCan("edit",$user,$title,"quick"))
-        {return false;}
-      if ($action=="move" && !$PermissionManager->userHasRight($user,"move") && $PermissionManager->userHasRight($user,"moveownuserpages") && $PermissionManager->userCan("edit",$user,$title,"quick"))
-        {return false;}
+
+    $allowed_actions=["delete","move"];
+    //$action should be checked before $PermissionManager->userCan to avoid "Allowed memory size of N bytes exhausted" error
+    if (in_array($action,$allowed_actions) && $title->getNamespace()==NS_USER && $title->getRootText()==$user->getName() && $PermissionManager->userCan("edit",$user,$title,"quick"))
+      {switch ($action)
+        {default:
+        if (!$PermissionManager->userHasRight($user,$action) && $PermissionManager->userHasRight($user,$action."ownuserpages"))
+          {return false;}
+        }
       }
     }
   }
 }
-?>
