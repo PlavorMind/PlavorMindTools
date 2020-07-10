2d26b1e65abe0e50bb9b2b4e93fbce3134da7777
PlavorSeol <pseol2190@outlook.com>
2020-07-11 08:58:01 +0900

    Rename a variable
    https://doc.wikimedia.org/mediawiki-core/master/php/interfaceMediaWiki_1_1Hook_1_1TitleIsAlwaysKnownHook.html

--- BlueCategoryLinks/includes/Hooks.php
+++ BlueCategoryLinks/includes/Hooks.php
@@ -3,13 +3,13 @@ namespace PlavorMind\PlavorMindTools\BlueCategoryLinks;
 use MediaWiki\MediaWikiServices;
 
 class Hooks implements \MediaWiki\Hook\TitleIsAlwaysKnownHook
-{public function onTitleIsAlwaysKnown($title,&$result)
+{public function onTitleIsAlwaysKnown($title,&$isKnown)
   {$pmt_config=MediaWikiServices::getInstance()->getConfigFactory()->makeConfig("plavormindtools");
 
   if (!$pmt_config->get("PMTFeatureConfig")["BlueCategoryLinks"]["enable"])
     {return;}
 
   if ($title->getNamespace() === NS_CATEGORY)
-    {$result=true;}
+    {$isKnown=true;}
   }
 }
