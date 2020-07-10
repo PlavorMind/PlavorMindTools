bfecf714df2ae37086317c79b4570a59a55d86c7
PlavorSeol <pseol2190@outlook.com>
2020-07-11 02:56:18 +0900

    Return earlier if tool is not enabled

--- NoActionsOnNonEditable/includes/Hooks.php
+++ NoActionsOnNonEditable/includes/Hooks.php
@@ -4,20 +4,22 @@ use MediaWiki\MediaWikiServices;
 
 class Hooks
 {public static function ongetUserPermissionsErrors($title,$user,$action,&$result)
-  {$config=MediaWikiServices::getInstance()->getConfigFactory()->makeConfig("noactionsonnoneditable");
+  {$pmt_config=MediaWikiServices::getInstance()->getConfigFactory()->makeConfig("plavormindtools");
 
-  if ($config->get("PMTFeatureConfig")["NoActionsOnNonEditable"]["enable"])
-    {$PermissionManager=MediaWikiServices::getInstance()->getPermissionManager();
+  if (!$pmt_config->get("PMTFeatureConfig")["NoActionsOnNonEditable"]["enable"])
+    {return;}
 
-    $denied_actions=["delete"];
-    if ($config->get("PMTFeatureConfig")["NoActionsOnNonEditable"]["HideMoveTab"])
-      {$denied_actions[]="move";}
-    //$action should be checked before $PermissionManager->userCan to avoid "Allowed memory size of N bytes exhausted" error
-    if (in_array($action,$denied_actions) && !$PermissionManager->userCan("edit",$user,$title,"quick"))
-      {switch ($action)
-        {default:
-        $result=["noactionsonnoneditable-cannot".$action."cannotedit"];}
-      return false;}
-    }
+  $config=MediaWikiServices::getInstance()->getConfigFactory()->makeConfig("noactionsonnoneditable");
+  $PermissionManager=MediaWikiServices::getInstance()->getPermissionManager();
+
+  $denied_actions=["delete"];
+  if ($config->get("PMTFeatureConfig")["NoActionsOnNonEditable"]["HideMoveTab"])
+    {$denied_actions[]="move";}
+  //$action should be checked before $PermissionManager->userCan to avoid "Allowed memory size of N bytes exhausted" error
+  if (in_array($action,$denied_actions) && !$PermissionManager->userCan("edit",$user,$title,"quick"))
+    {switch ($action)
+      {default:
+      $result=["noactionsonnoneditable-cannot".$action."cannotedit"];}
+    return false;}
   }
 }
