3b368af6ae3f55c817d0b4be4a754800ecf5d782
PlavorSeol <pseol2190@outlook.com>
2020-07-11 03:57:56 +0900

    Use hook interface

--- BlueCategoryLinks/includes/Hooks.php
+++ BlueCategoryLinks/includes/Hooks.php
@@ -2,8 +2,8 @@
 namespace PlavorMind\PlavorMindTools\BlueCategoryLinks;
 use MediaWiki\MediaWikiServices;
 
-class Hooks
-{public static function onTitleIsAlwaysKnown($title,&$result)
+class Hooks implements \MediaWiki\Hook\TitleIsAlwaysKnownHook
+{public function onTitleIsAlwaysKnown($title,&$result)
   {$pmt_config=MediaWikiServices::getInstance()->getConfigFactory()->makeConfig("plavormindtools");
 
   if (!$pmt_config->get("PMTFeatureConfig")["BlueCategoryLinks"]["enable"])
