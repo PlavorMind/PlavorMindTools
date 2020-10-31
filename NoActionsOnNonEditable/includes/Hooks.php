80f142e8a06e0cd1db2f702296e04f5093e5f7df
PlavorSeol <pseol2190@outlook.com>
2020-10-31 20:57:22 +0900

    Use injected PermissionManager service

--- NoActionsOnNonEditable/includes/Hooks.php
+++ NoActionsOnNonEditable/includes/Hooks.php
@@ -1,22 +1,26 @@
 <?php
 namespace PlavorMind\PlavorMindTools\NoActionsOnNonEditable;
 use MediaWiki\MediaWikiServices;
+use MediaWiki\Permissions\PermissionManager;
 
 class Hooks implements \MediaWiki\Permissions\Hook\GetUserPermissionsErrorsHook
-{public function onGetUserPermissionsErrors($title,$user,$action,&$result)
+{private $PermissionManager;
+
+public function __construct(PermissionManager $PermissionManager)
+  {$this->PermissionManager=$PermissionManager;}
+
+public function onGetUserPermissionsErrors($title,$user,$action,&$result)
   {$pmt_config=MediaWikiServices::getInstance()->getConfigFactory()->makeConfig("plavormindtools");
 
   if (!$pmt_config->get("PMTFeatureConfig")["NoActionsOnNonEditable"]["enable"])
     {return;}
 
   $config=MediaWikiServices::getInstance()->getConfigFactory()->makeConfig("noactionsonnoneditable");
-  $PermissionManager=MediaWikiServices::getInstance()->getPermissionManager();
 
   $denied_actions=["delete"];
   if ($config->get("PMTFeatureConfig")["NoActionsOnNonEditable"]["HideMoveTab"])
     {$denied_actions[]="move";}
-  //$action should be checked before $PermissionManager->userCan to avoid "Allowed memory size of N bytes exhausted" error
-  if (in_array($action,$denied_actions) && !$PermissionManager->userCan("edit",$user,$title,"quick"))
+  if (!$this->PermissionManager->userCan("edit",$user,$title,"quick") && in_array($action,$denied_actions))
     {switch ($action)
       {default:
       $result=["noactionsonnoneditable-cannot".$action."cannotedit"];}
