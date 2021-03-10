b2c268f1192d803cc21348c903f54dd9bbaf640d
PlavorSeol <pseol2190@outlook.com>
2021-03-10 15:11:17 +0900

    Apply new coding conventions

--- NoActionsOnNonEditable/includes/Hooks.php
+++ NoActionsOnNonEditable/includes/Hooks.php
@@ -4,26 +4,28 @@ use MediaWiki\MediaWikiServices;
 use MediaWiki\Permissions\PermissionManager;
 
 class Hooks implements \MediaWiki\Permissions\Hook\GetUserPermissionsErrorsHook
-{private $PermissionManager;
+  {private $PermissionManager;
 
-public function __construct(PermissionManager $PermissionManager)
-  {$this->PermissionManager=$PermissionManager;}
+  public function __construct(PermissionManager $PermissionManager)
+    {$this->PermissionManager=$PermissionManager;}
 
-public function onGetUserPermissionsErrors($title,$user,$action,&$result)
-  {$pmt_config=MediaWikiServices::getInstance()->getConfigFactory()->makeConfig("plavormindtools");
+  public function onGetUserPermissionsErrors($title, $user, $action, &$result)
+    {$pmt_config=MediaWikiServices::getInstance()->getConfigFactory()->makeConfig('plavormindtools');
 
-  if (!$pmt_config->get("PMTFeatureConfig")["NoActionsOnNonEditable"]["enable"])
-    {return;}
+    if (!$pmt_config->get('PMTFeatureConfig')['NoActionsOnNonEditable']['enable'])
+      {return;}
 
-  $config=MediaWikiServices::getInstance()->getConfigFactory()->makeConfig("noactionsonnoneditable");
+    $config=MediaWikiServices::getInstance()->getConfigFactory()->makeConfig('noactionsonnoneditable');
 
-  $denied_actions=["delete"];
-  if ($config->get("PMTFeatureConfig")["NoActionsOnNonEditable"]["HideMoveTab"])
-    {$denied_actions[]="move";}
-  if (in_array($action,$denied_actions) && !$this->PermissionManager->userCan("edit",$user,$title,"quick"))
-    {switch ($action)
-      {default:
-      $result=["noactionsonnoneditable-cannot".$action."cannotedit"];}
-    return false;}
+    $denied_actions=['delete'];
+
+    if ($config->get('PMTFeatureConfig')['NoActionsOnNonEditable']['HideMoveTab'])
+      {$denied_actions[]='move';}
+
+    if (in_array($action, $denied_actions) && !$this->PermissionManager->userCan('edit', $user, $title, 'quick'))
+      {switch ($action)
+        {default:
+        $result=["noactionsonnoneditable-cannot{$action}cannotedit"];}
+      return false;}
+    }
   }
-}
