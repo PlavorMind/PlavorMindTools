bb97f40e96e1faa671e1ab484b199cd802a22a1b
PlavorSeol <pseol2190@outlook.com>
2022-09-03 00:38:32 +0900

    Migrate existing hook handler
    From includes/Hooks.php to src/HookHandler.php

--- src/HookHandler.php
+++ src/HookHandler.php
@@ -1,8 +1,10 @@
 <?php
-namespace PlavorMind\PlavorMindToolsNew;
+namespace PlavorMind\PlavorMindTools;
 use MediaWiki\Hook\MediaWikiServicesHook;
+use MediaWiki\MediaWikiServices;
+use MediaWiki\Permissions\Hook\UserGetAllRightsHook;
 
-class HookHandler implements MediaWikiServicesHook {
+class HookHandler implements MediaWikiServicesHook, UserGetAllRightsHook {
   // MediaWikiServices type should not be specified for $services parameter.
   public function onMediaWikiServices($services) {
     global $wgAddGroups, $wgGroupPermissions, $wgGroupsAddToSelf, $wgGroupsRemoveFromSelf, $wgRemoveGroups, $wgRevokePermissions;
@@ -11,5 +13,19 @@ class HookHandler implements MediaWikiServicesHook {
     foreach ($groups as $group) {
       unset($wgAddGroups[$group], $wgGroupPermissions[$group], $wgGroupsAddToSelf[$group], $wgGroupsRemoveFromSelf[$group], $wgRemoveGroups[$group], $wgRevokePermissions[$group]);
     }
+  }
+
+  public function onUserGetAllRights(&$rights) {
+    $settings = MediaWikiServices::getInstance()->getMainConfig();
+
+    if (!$settings->get('PMTFeatureConfig')['UserPageAccess']['enable']) {
+      return;
+    }
+
+    $rights = array_merge($rights, [
+      'deleteownuserpages',
+      'editotheruserpages',
+      'moveownuserpages'
+    ]);
   }
 }
