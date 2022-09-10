1086ffa5f4d4b0ca25e0978c4d55e44b28c714fa
PlavorSeol <pseol2190@outlook.com>
2022-09-10 23:12:44 +0900

    Update HookHandler.php
    * Remove UserGetAllRights hook handler
    * Rename $wgPMTDisabledUserGroups setting to $wgPMTDisableUserGroups

--- src/HookHandler.php
+++ src/HookHandler.php
@@ -1,31 +1,15 @@
 <?php
 namespace PlavorMind\PlavorMindTools;
 use MediaWiki\Hook\MediaWikiServicesHook;
-use MediaWiki\MediaWikiServices;
-use MediaWiki\Permissions\Hook\UserGetAllRightsHook;
 
-class HookHandler implements MediaWikiServicesHook, UserGetAllRightsHook {
+class HookHandler implements MediaWikiServicesHook {
   // MediaWikiServices type should not be specified for $services parameter.
   public function onMediaWikiServices($services) {
     global $wgAddGroups, $wgGroupPermissions, $wgGroupsAddToSelf, $wgGroupsRemoveFromSelf, $wgRemoveGroups, $wgRevokePermissions;
-    $groups = $services->getMainConfig()->get('PMTDisabledUserGroups');
+    $groups = $services->getMainConfig()->get('PMTDisableUserGroups');
 
     foreach ($groups as $group) {
       unset($wgAddGroups[$group], $wgGroupPermissions[$group], $wgGroupsAddToSelf[$group], $wgGroupsRemoveFromSelf[$group], $wgRemoveGroups[$group], $wgRevokePermissions[$group]);
     }
-  }
-
-  public function onUserGetAllRights(&$rights) {
-    $settings = MediaWikiServices::getInstance()->getMainConfig();
-
-    if (!$settings->get('PMTFeatureConfig')['UserPageAccess']['enable']) {
-      return;
-    }
-
-    $rights = array_merge($rights, [
-      'deleteownuserpages',
-      'editotheruserpages',
-      'moveownuserpages'
-    ]);
   }
 }
