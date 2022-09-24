b965d085c294024434bbbb4df64a604715919178
PlavorSeol <pseol2190@outlook.com>
2022-09-24 18:59:41 +0900

    Remove unnecessary comment about specifying the type in the parameter
    Types should not be specified for hook handler functions at all if hook handler class implements hook interfaces.

--- src/HookHandler.php
+++ src/HookHandler.php
@@ -3,7 +3,6 @@ namespace PlavorMind\PlavorMindTools;
 use MediaWiki\Hook\MediaWikiServicesHook;
 
 class HookHandler implements MediaWikiServicesHook {
-  // MediaWikiServices type should not be specified for $services parameter.
   public function onMediaWikiServices($services) {
     global $wgAddGroups, $wgGroupPermissions, $wgGroupsAddToSelf, $wgGroupsRemoveFromSelf, $wgRemoveGroups, $wgRevokePermissions;
     $groups = $services->getMainConfig()->get('PMTDisableUserGroups');
