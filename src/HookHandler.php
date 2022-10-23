a761ac3e336077a38cf68929e7b3383d63a822de
PlavorSeol <pseol2190@outlook.com>
2022-10-23 20:21:04 +0900

    Add a feature for always enabling new layout for Special:Preferences

--- src/HookHandler.php
+++ src/HookHandler.php
@@ -1,14 +1,23 @@
 <?php
 namespace PlavorMind\PlavorMindTools;
 use MediaWiki\Hook\MediaWikiServicesHook;
+use MediaWiki\Hook\PreferencesGetLayoutHook;
+use MediaWiki\MediaWikiServices;
 
-class HookHandler implements MediaWikiServicesHook {
+class HookHandler implements MediaWikiServicesHook, PreferencesGetLayoutHook {
   public function onMediaWikiServices($services) {
     global $wgAddGroups, $wgGroupPermissions, $wgGroupsAddToSelf, $wgGroupsRemoveFromSelf, $wgRemoveGroups, $wgRevokePermissions;
     $groups = $services->getMainConfig()->get('PMTDisableUserGroups');
 
     foreach ($groups as $group) {
       unset($wgAddGroups[$group], $wgGroupPermissions[$group], $wgGroupsAddToSelf[$group], $wgGroupsRemoveFromSelf[$group], $wgRemoveGroups[$group], $wgRevokePermissions[$group]);
+    }
+  }
+
+  // 1.40+
+  public function onPreferencesGetLayout(&$useMobileLayout, $skin) {
+    if (MediaWikiServices::getInstance()->getMainConfig()->get('PMTNewPreferencesLayout')) {
+      $useMobileLayout = true;
     }
   }
 }
