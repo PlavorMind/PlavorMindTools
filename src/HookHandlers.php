5a67e715f7da3d4f57dd3a81411fa9d8f29a685e
PlavorSeol <pseol2190@outlook.com>
2023-02-18 03:10:39 +0900

    Add support for applying CSP to img_auth.php

--- src/HookHandlers.php
+++ src/HookHandlers.php
@@ -1,12 +1,31 @@
 <?php
 namespace PlavorMind\PlavorMindTools;
+use MediaWiki\Hook\ImgAuthModifyHeadersHook;
 use MediaWiki\Hook\PreferencesGetLayoutHook;
 use MediaWiki\MediaWikiServices;
 
-class HookHandlers implements PreferencesGetLayoutHook {
+class HookHandlers implements ImgAuthModifyHeadersHook, PreferencesGetLayoutHook {
+  private $settings;
+
+  public function __construct() {
+    $this->settings = MediaWikiServices::getInstance()->getMainConfig();
+  }
+
+  public function onImgAuthModifyHeaders($title, &$headers) {
+    $policies = $this->settings->get('PMTImgAuthCSPs');
+
+    if ($policies['enforced'] !== null) {
+      $headers['Content-Security-Policy'] = $policies['enforced'];
+    }
+
+    if ($policies['report-only'] !== null) {
+      $headers['Content-Security-Policy-Report-Only'] = $policies['report-only'];
+    }
+  }
+
   // 1.40+
   public function onPreferencesGetLayout(&$useMobileLayout, $skinName, $skinProperties = []) {
-    if (MediaWikiServices::getInstance()->getMainConfig()->get('PMTNewPreferencesLayout')) {
+    if ($this->settings->get('PMTNewPreferencesLayout')) {
       $useMobileLayout = true;
     }
   }
