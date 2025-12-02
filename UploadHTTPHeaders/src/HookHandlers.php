e10a5a00d68600b6869c2005f1751a0f9d551fae
PlavorSeol <pseol2190@outlook.com>
2025-12-02 13:26:38 +0900

    Update UploadHTTPHeaders tool
    * Remove img_auth.php handling
    * Use built-in ContentSecurityPolicy::getMediaHeader() instead
    * Split the detection of viewing an uploaded file that is deleted to Utils::getDeletedUploadName()

--- UploadHTTPHeaders/src/HookHandlers.php
+++ UploadHTTPHeaders/src/HookHandlers.php
@@ -2,80 +2,29 @@
 declare(strict_types = 1);
 
 namespace PlavorMind\PlavorMindTools\UploadHTTPHeaders;
-use MediaWiki\Config\Config;
-use MediaWiki\Hook\ImgAuthModifyHeadersHook;
+use MediaWiki\Request\ContentSecurityPolicy;
 use MediaWiki\SpecialPage\Hook\SpecialPageBeforeExecuteHook;
-use MediaWiki\Title\Title;
 
-class HookHandlers implements ImgAuthModifyHeadersHook, SpecialPageBeforeExecuteHook {
-  private Config $settings;
-
-  public function __construct(Config $settings) {
-    $this->settings = $settings;
-  }
-
-  public function onImgAuthModifyHeaders($title, &$headers) {
-    if (!$this->settings->get('UHHEnable')) {
-      return;
-    }
-
-    $CSPs = $this->settings->get('UHHCSPs');
-
-    if ($CSPs['enforced'] !== null) {
-      $headers['Content-Security-Policy'] = $CSPs['enforced'];
-    }
-
-    if ($CSPs['report-only'] !== null) {
-      $headers['Content-Security-Policy-Report-Only'] = $CSPs['report-only'];
-    }
-  }
-
+class HookHandlers implements SpecialPageBeforeExecuteHook {
   public function onSpecialPageBeforeExecute($special, $subPage) {
-    $settings = $special->getConfig();
-
-    if (!($settings->get('UHHEnable') && $special->getName() === 'Undelete')) {
+    if (!($special->getConfig()->get('UHHEnable') && $special->getName() === 'Undelete')) {
       return;
     }
 
     $request = $special->getRequest();
+    $filename = Utils::getDeletedUploadName($request, $subPage);
 
-    if ($request->getMethod() !== 'GET') {
+    if ($filename === null) {
       return;
     }
 
-    $fileParameter = $request->getRawVal('file');
+    $CSP = ContentSecurityPolicy::getMediaHeader($filename);
 
-    if ($fileParameter === null || $fileParameter === '') {
+    if ($CSP === null) {
       return;
     }
 
-    $token = $request->getRawVal('token');
-
-    if ($token === null || $token === '') {
-      return;
-    }
-
-    $titleString = ($subPage === null) || ($subPage === '') ? $request->getText('target') : $subPage;
-
-    if ($titleString === '') {
-      return;
-    }
-
-    $title = Title::newFromText($titleString);
-
-    if ($title === null || !$title->inNamespace(NS_FILE)) {
-      return;
-    }
-
-    $CSPs = $settings->get('UHHCSPs');
-    $response = $request->response();
-
-    if ($CSPs['enforced'] !== null) {
-      $response->header('Content-Security-Policy: ' . $CSPs['enforced']);
-    }
-
-    if ($CSPs['report-only'] !== null) {
-      $response->header('Content-Security-Policy-Report-Only: ' . $CSPs['report-only']);
-    }
+    $CSPheader = 'Content-Security-Policy: ' . $CSP;
+    $request->response()->header($CSPheader);
   }
 }
